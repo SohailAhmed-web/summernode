c1d554a1b97d0303d9094f9b29872c08f8b0a1c9
SohailAhmed-web <sohail.bscsf19@iba-suk.edu.pk>
2024-06-06 16:29:56 +0500

    event emitter added

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+//initialize object
+const myEmitter = new MyEmitter();
+
+//add listener for log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+    myEmitter.emit('log', 'Log event emitted!');
+}, 2000);
+
+
 const {format} = require('date-fns');
 const {v4: uuid} = require('uuid');
  
